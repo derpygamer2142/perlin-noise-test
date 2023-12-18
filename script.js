46e65a5ce93b4b65d1f1910b21ca269078e651e3
derpygamer2142 <derpygamer2142@gmail.com>
2023-12-18 12:20:54 -0800

    fps detector

--- script.js
+++ script.js
@@ -19,8 +19,27 @@ let noiseLevels = noise.generateNoise(amount,a)
 let camX = 0;
 let camY = 0;
 
-function draw() {
+let lastTime = Date.now()
+let elapsedTime = 0
+let fps = []; // gotta average it out because annoying
 
+function fpsManager(dt) {
+    fps.push(1/dt)
+    if (fps.length > 100) {
+        fps.reverse();
+        fps.pop();
+        fps.reverse();
+    }
+
+    let val = 0
+    fps.forEach(f => {
+        val += f
+    })
+    return val/fps.length
+}
+
+function draw() {
+    elapsedTime = (Date.now() - lastTime)/1000
     if (input.ArrowDown || input.ArrowUp) {
         if (input.ArrowUp) {a += 0.04}
         if (input.ArrowDown) {a -= 0.04}
@@ -34,14 +53,22 @@ function draw() {
     ctx.clearRect(0,0,WIDTH,HEIGHT);
     ctx.fillStyle = "rgb(60,60,60)";
     ctx.fillRect(0,0,WIDTH,HEIGHT);
-    ctx.strokeStyle = "brown";
     ctx.lineWidth = 2
 
     for (let x = 0; x < noiseLevels.length-1; x++) {
+        if (x-camX < 0) {continue;}
         if (x-camX > WIDTH) {break;}
+        
+        ctx.strokeStyle = "saddlebrown"
+        ctx.beginPath();
+        ctx.moveTo(x-camX,((HEIGHT/2)+noiseLevels[x])-camY+15)
+        ctx.lineTo(x-camX,500-camY)
+        ctx.stroke();
+
+        ctx.strokeStyle = "green";
         ctx.beginPath();
         ctx.moveTo(x-camX,((HEIGHT/2)+noiseLevels[x])-camY)
-        ctx.lineTo((x+1)-camX,((HEIGHT/2)+noiseLevels[x+1])-camY)
+        ctx.lineTo(x-camX,((HEIGHT/2)+noiseLevels[x])-camY+15)
         ctx.stroke();
     }
 
@@ -49,6 +76,8 @@ function draw() {
     ctx.fillStyle = "black"
     ctx.fillText(`Amplitude: ${a}`,0,HEIGHT*0.04);
     ctx.fillText(`Pos: ${camX},${camY}`,0,HEIGHT*0.08)
+    ctx.fillText(`FPS: ${fpsManager(elapsedTime)}`,0,HEIGHT*0.12)
+    lastTime = Date.now()
 }
 
 setInterval(draw,17);
